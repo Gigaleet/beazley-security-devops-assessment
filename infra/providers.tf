89cb2b680effc12787fa60671bd5232c431d2747
David <David@gigaleet.com>
2025-06-16 10:19:53 -0400

    Setup Terraform backend and providers

---
+++ infra/providers.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_version = ">= 1.11.0"
+  
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.18.0"
+    }
+  }
+
+  backend "s3" {
+    bucket       = "beazley-infra"
+    key          = "envs/prod/terraform.tfstate"
+    region       = "us-east-1"
+    encrypt      = true
+    use_lockfile = true
+  }
+}
+
+provider "aws" {
+  region = var.aws_region
+}
